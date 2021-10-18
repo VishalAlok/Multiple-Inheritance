d31ff02225e189f263732c6195e9c0d42b41bb15
vishal.Alok <you@example.com>
2021-10-18 06:09:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multiple_Inheritance
+{
+    public interface IA //ineterface  1    
+    {
+        string setImgs(string a);
+    }
+    public interface IB  //Interface 2    
+    {
+        int getAmount(int Amt);
+    }
+    public class Car : IA, IB //implementatin    
+    {
+        public int getAmount(int Amt)
+        {
+            return 100;
+        }
+        public string setImgs(string a)
+        {
+            return "this is the car";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.getAmount(1000);
+            c.setImgs("Text");
+        }
+    }
+}
